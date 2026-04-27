57cb659953f2921abc27aca45a65ae0a72306a63
Kayra Uylar <k.uylar@outlook.com>
2026-04-27 23:56:56 +0300

    add tabs to do later

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,5 @@
 import "./Settings.css";
-import {Users, Video, Zap} from "react-feather";
+import {Film, Link2, Users, Video, Zap} from "react-feather";
 import {NavLink, Outlet} from "react-router-dom";
 
 
@@ -10,6 +10,8 @@ export default function Settings() {
 				<NavLink to={"transcode"} className={"settingsCategory"}><Video/><span>Transcoding</span></NavLink>
 				<NavLink to={"benchmark"} className={"settingsCategory"}><Zap/><span>Benchmark</span></NavLink>
 				<NavLink to={"users"} className={"settingsCategory"}><Users/><span>Users</span></NavLink>
+				<NavLink to={"libraries"} className={"settingsCategory"}><Film/><span>Libraries</span></NavLink>
+				<NavLink to={"webhooks"} className={"settingsCategory"}><Link2/><span>Webhooks</span></NavLink>
 			</div>
 			<div className={"settingsItems"}>
 				<Outlet/>
