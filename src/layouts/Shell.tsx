ad29f95816711e2aefb7f318af2a58686e85a12a
Kayra Uylar <k.uylar@outlook.com>
2026-06-05 20:41:35 +0300

    move the navigation to the sidebar

--- src/layouts/Shell.tsx
+++ src/layouts/Shell.tsx
@@ -1,27 +1,37 @@
 import "./Shell.css";
-import {NavLink, Outlet, useNavigate} from "react-router-dom";
+import {NavLink, Outlet, useLocation, useNavigate} from "react-router-dom";
 import FlexDivider from "../components/FlexDivider.tsx";
 import {useAuth} from "../auth/AuthContext.tsx";
 import UserIndicator from "../components/UserIndicator.tsx";
+import type {ReactNode} from "react";
+import {HomeIcon, LibraryIcon, SettingsIcon} from "lucide-react";
+
+function NavButton({to, label, icon}: { to: string, label: string, icon: ReactNode }) {
+	return (<NavLink className={"app-root__nav__link"} to={to}>{icon}<span>{label}</span></NavLink>);
+}
 
 export default function Shell() {
 	const auth = useAuth();
 	const user = auth.user;
 	const navigate = useNavigate();
+	const location = useLocation();
+	const showNavigation = ["", "library", "serverSettings"].includes(location.pathname.split('/').slice(1)[0]);
 
 	return (
 		<div className={"app-root"}>
 			<header className={"app-root__header"}>
 				<div className={"app-root__header__logo"}>OwnStream</div>
-				<span>&bull;</span>
-				<NavLink to={"/"} className={"app-root__header__link"}>Home</NavLink>
-				<NavLink to={"/library"} className={"app-root__header__link"}>Library</NavLink>
-				{auth.showSettings() &&
-					<NavLink to={"/serverSettings"} className={"app-root__header__link"}>Settings</NavLink>}
 				<FlexDivider/>
 				<UserIndicator user={user} onClick={() => navigate("/profiles")}/>
 			</header>
-			<main className={"app-root__main"}>
+			{showNavigation &&
+				<nav className={"app-root__nav"}>
+					<NavButton to={"/"} label={"Home"} icon={<HomeIcon/>}/>
+					<NavButton to={"/library"} label={"Library"} icon={<LibraryIcon/>}/>
+					{auth.showSettings() &&
+						<NavButton to={"/serverSettings"} label={"Settings"} icon={<SettingsIcon/>}/>}
+				</nav>}
+			<main className={`app-root__main ${showNavigation && "hasNav"}`}>
 				<Outlet/>
 			</main>
 		</div>
